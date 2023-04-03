11c7831fa7d8e129cafde94a654941fec5566e82
Admin1 <a89086829981@gmail.com>
2023-04-03 06:48:32 +0300

    Add ShopItemActivity

---
+++ app/src/main/java/com/ak87/simpleshoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.ak87.simpleshoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ak87.simpleshoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
